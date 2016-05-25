04edd2f841d640231f9014abdb787cad18c8f407
bombadillo <mumfordnsons@gmail.com>
2016-05-25 23:35:48 +0100

    Update WebApiConfig.cs
    Set response type to JSON unless the accept header is text/xml

--- NinjaDomain.WebApi/App_Start/WebApiConfig.cs
+++ NinjaDomain.WebApi/App_Start/WebApiConfig.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-
-namespace NinjaDomain.WebApi
+﻿namespace NinjaDomain.WebApi
 {
+    using System.Net.Http.Headers;
+    using System.Web.Http;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -13,7 +11,7 @@ namespace NinjaDomain.WebApi
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
